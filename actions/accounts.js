b834962ed594f341f0f63a6a63fb15f21b2dfa6a
Muhammad-Sheik-Nauman <noumansheik07@gmail.com>
2025-09-16 23:50:42 +0530

    Refactor code structure for improved readability and maintainability

--- actions/accounts.js
+++ actions/accounts.js
@@ -46,7 +46,38 @@ export async function updateDefaultAccount(accountId) {
     } catch (error) {
         return { success: false, error: error.message };
     }
+}
+export async function getAccountWithTransactions(accountId) {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+    const user = await db.user.findUnique({
+        where: { clerkUserId: userId },
+    });
 
+    if (!user) {
+        throw new Error("User not found");
+    }
+    if (!user) {
+        throw new Error("User NotFound")
+    }
+    const account = await db.account.findUnique({
+        where: { id: accountId, userId: user.id },
+         include: {
+      transactions: {
+        orderBy: { date: "desc" },
+      },
+      _count: {
+        select: { transactions: true },
+      },
+    },
+  });
+    if (!account) return null;
 
+  return {
+    ...serializeTransaction(account),
+    transactions: account.transactions.map(serializeTransaction),
+  };
 
 }
+
+
---
+++ app/(main)/account/components/Transactions.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { Checkbox } from '@radix-ui/react-checkbox'
+
+const TransactionsTable = ({transactions}) => {
+  return (
+    <div className='space-y-4'>
+        {/* filters */}
+        {/* transactions */}
+        <div className='rounded-md border'>
+        <Table>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[50px]">
+        <Checkbox/>
+      </TableHead>
+       <TableHead className="cursor-pointer">
+        Date
+      </TableHead>
+      
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+</div>
+    </div>
+  )
+}
+
+export default TransactionsTable
