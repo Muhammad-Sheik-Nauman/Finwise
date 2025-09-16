400724ce1f11020e06276ecf5273b5ab0e850385
Muhammad-Sheik-Nauman <noumansheik07@gmail.com>
2025-09-16 12:58:19 +0530

    feat: add new layout and page components for dashboard, account, and transaction sections; implement NotFound page; update Prisma schema and migration for user ID changes

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ function DashboardPage(){
+  return (
+    <div className='px-5'>
+        
+             <div className='px-5'>Page</div>
+        
+        
+    </div>
+  )
+};
+
+export default DashboardPage;
--- components/header.jsx
+++ components/header.jsx
@@ -18,7 +18,7 @@ import { checkUser } from '@/lib/checkUser'
 
 
 const Header = async () => {
-  const user = await checkUser();
+  await checkUser();
   return (
     <div className='fixed top- w-full bg-white/80 backdrop-blur-md z-50 border-b'>
       <nav className='container mx-auto px-4 py-1 flex items-center justify-between'>
