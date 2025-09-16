ba39e7e773cd5f26e4fc4fd3a0dc26310ccfb33d
Muhammad-Sheik-Nauman <noumansheik07@gmail.com>
2025-09-16 16:31:52 +0530

    feat: enhance CreateAccountDrawer with form validation and UI components; add account schema for validation

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import CreateAccountDrawer from '@/components/create-account-drawer';
+import { Card, CardContent } from '@/components/ui/card';
+import { Plus } from 'lucide-react';
 
  function DashboardPage(){
   return (
@@ -7,9 +9,14 @@ import CreateAccountDrawer from '@/components/create-account-drawer';
         {/* budget progress */}
         {/* Dashboard Overview */}
         {/* accounts grid */} 
-          <div>
+          <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
             <CreateAccountDrawer>
-                open
+                <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+                    <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+                        <Plus className='h-10 w-10 mb-2'/>
+                        <p className='text-sm font-medium '>Add New Account</p>
+                    </CardContent>
+                </Card>
             </CreateAccountDrawer>
           </div>
     </div>
--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -4,24 +4,76 @@ import { Drawer } from "./ui/drawer"
 import { DrawerTrigger } from "./ui/drawer"
 import { DrawerContent } from "./ui/drawer"
 import { DrawerHeader } from "./ui/drawer"
-import { DrawerTitle } from "./ui/drawer"  
-import { useState } from "react" 
+import { DrawerTitle } from "./ui/drawer"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { accountSchema } from "@/app/lib/schema"
+import { Input } from "./ui/input"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
 
 
 
-const CreateAccountDrawer = ({children}) => {
+const CreateAccountDrawer = ({ children }) => {
 
-const[open,setOpen]=useState(false);
-  return (
-    <Drawer open={open} onOpenChange={setOpen}>
-  <DrawerTrigger>{children}</DrawerTrigger>
-  <DrawerContent>
-    <DrawerHeader>
-      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-    </DrawerHeader>
-  </DrawerContent>
-</Drawer>
-  )
+    const [open, setOpen] = useState(false);
+
+    const { register, handleSubmit, formState: { errors }, setValue, watch, reset } = useForm({
+        resolver: zodResolver(accountSchema),
+        defaultValues: {
+            name: "",
+            type: "CURRENT",
+            balance: "",
+            isDefault: false,
+        },
+    });
+
+
+    return (
+        <Drawer open={open} onOpenChange={setOpen}>
+            <DrawerTrigger asChild>{children}</DrawerTrigger>
+            <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>Create New Account</DrawerTitle>
+                </DrawerHeader>
+                <div className="px-4 pb-4">
+                    <form>
+                        <div className="space-y-2">
+                            <label htmlFor="name" className="text-sm font-medium">Account Name</label>
+                            <Input id="name" placeholder="e.g., Main Checking"
+                                {...register("name")}
+                            />
+                            {errors.name && (
+                                <p className="text-sm text-red-500">{errors.name.message}</p>
+                            )}
+                        </div>
+                    </form>
+                </div>
+
+
+                <div className="px-4 pb-4">
+                    <form>
+                        <div className="space-y-2">
+                            <label htmlFor="type" className="text-sm font-medium">Account Type</label>
+                            <Select onValueChange={(value)=>setValue("type",value)} defaultValue={watch("type")}>
+                                <SelectTrigger id="type">
+                                    <SelectValue placeholder="Select Type" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="CURRENT">Current</SelectItem>
+                                    <SelectItem value="SAVINGS">Savings</SelectItem>
+                                    
+                                </SelectContent>
+                            </Select>
+                            {errors.type && (
+                                <p className="text-sm text-red-500">{errors.type.message}</p>
+                            )}
+                        </div>
+                    </form>
+                </div>
+            </DrawerContent>
+        </Drawer>
+    )
 }
 
 export default CreateAccountDrawer
