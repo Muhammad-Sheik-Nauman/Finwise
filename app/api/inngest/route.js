9e07ffeebbfd17cbb846568b4d1ae903d8012c51
Muhammad-Sheik-Nauman <noumansheik07@gmail.com>
2025-09-21 22:03:23 +0530

    feat: implement checkBudgetAlert function to monitor budget usage and send alerts; update API route to include the function

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,4 +1,5 @@
 import { inngest } from "@/lib/inngest/client";
+import { checkBudgetAlert } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 
@@ -6,8 +7,5 @@ import { serve } from "inngest/next";
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-    
-  ],
+  functions: [checkBudgetAlert  ],
 });
