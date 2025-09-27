0a482120c54af5922dd668cb360878c7689c1b30
Muhammad-Sheik-Nauman <noumansheik07@gmail.com>
2025-09-27 22:58:03 +0530

    feat: enhance AddTransactionForm with improved UI components and validation; add account selection and category filtering

--- app/(main)/transaction/_components/transaction-form.jsx
+++ app/(main)/transaction/_components/transaction-form.jsx
@@ -6,6 +6,10 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import useFetch from "@/hooks/use-fetch";
 import { createTransaction } from "@/actions/transaction";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import CreateAccountDrawer from "@/components/create-account-drawer";
+
 import {
   Select,
   SelectContent,
@@ -14,8 +18,15 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 const AddTransactionForm = ({ accounts, categories }) => {
-
-  useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setValue,
+    getValues,
+    reset,
+  } = useForm({
     resolver: zodResolver(transactionSchema),
     defaultValues: {
       type: "EXPENSE",
@@ -33,23 +44,114 @@ const AddTransactionForm = ({ accounts, categories }) => {
     data: transactionResult,
   } = useFetch(createTransaction);
 
+  const type = watch("type");
+  const isRecurring = watch("isRecurring");
+  const date = watch("date");
+
+  const filteredCategories = categories.filter(
+    (category) => category.type === type
+  );
+
   return (
-    <form>
+    <form className='space-y-6'>
       {/* ai receipt scanner */}
 
-      <div>
-        <label>Type</label>
-        <Select>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Theme" />
+      <div className='space-y-2' >
+        <label className='text-sm font-medium'>Type</label>
+        <Select
+          onValueChange={(value) => setValue("type", value)}
+          defaultValue={type}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Select Type" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="light">Light</SelectItem>
-            <SelectItem value="dark">Dark</SelectItem>
-            <SelectItem value="system">System</SelectItem>
+            <SelectItem value="EXPENSE">Expense</SelectItem>
+            <SelectItem value="INCOME">Income</SelectItem>
+
           </SelectContent>
         </Select>
+
+        {errors.type && (
+          <p className="text-sm text-red-500">{errors.type.message}</p>
+        )}
       </div>
+
+      <div className='grid gap-6 md:grid-cols-2' >
+        <div className='space-y-2' >
+          <label className='text-sm font-medium'>Amount</label>
+          <Input
+            type="number"
+            step="0.01"
+            placeholder="0.00"
+            {...register("amount")}
+          />
+
+
+          {errors.amount && (
+            <p className="text-sm text-red-500">{errors.type.message}</p>
+          )}
+        </div>
+        <div className='space-y-2' >
+          <label className='text-sm font-medium'>Account</label>
+          <Select
+            onValueChange={(value) => setValue("accountId", value)}
+            defaultValue={getValues("accountId")}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select Account" />
+            </SelectTrigger>
+            <SelectContent>
+              {accounts.map((account) => (
+                <SelectItem key={account.id} value={account.id}>
+                  {account.name} (${parseFloat(account.balance).toFixed(2)})
+                </SelectItem>
+              ))}
+
+              <CreateAccountDrawer>
+                <Button
+                  variant="ghost"
+                  className="relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none hover:bg-accent hover:text-accent-foreground"
+                >
+                  Create Account
+                </Button>
+              </CreateAccountDrawer>
+
+
+            </SelectContent>
+          </Select>
+
+          {errors.type && (
+            <p className="text-sm text-red-500">{errors.type.message}</p>
+          )}
+        </div>
+      </div>
+
+      <div className="space-y-2">
+        <label className="text-sm font-medium">Category</label>
+        <Select
+          onValueChange={(value) => setValue("category", value)}
+          defaultValue={getValues("category")}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Select category" />
+          </SelectTrigger>
+          <SelectContent>
+            {filteredCategories.map((category) => (
+              <SelectItem key={category.id} value={category.id}>
+                {category.name}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        {errors.category && (
+          <p className="text-sm text-red-500">{errors.category.message}</p>
+        )}
+      </div>
+
+
+
+
 
     </form>
   )
